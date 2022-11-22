0897a352814ff17e479311c2d8784a89906064a8
MoermansJ <Jonathan.moermans2@gmail.com>
2022-11-22 11:53:12 +0100

    main page done

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -17,26 +17,25 @@ function Main() {
         </div>
 
         <div className="text">
+          At Listt we provide the appropriate technology solutions for{" "}
+          <span className="bold">regeneration</span>, prioritising soil health
+          while building thriving food production businesses.
+        </div>
+
+        <div className="text">
           In order to better understand the type of problems we could be
           solving, Listt is working with Innovate UK to explore and develop{" "}
           <span className="bold">autonomous soil health monitoring </span>{" "}
           systems by integrating low cost hardware with widely available
-          software stacks.
+          software stacks. From traditional IT solutions to connected sensors
+          for measuring soil and crop health, or even advanced automation using
+          robotics.
         </div>
 
         <div className="text">
-          Healthy soils produce healthy food and require less inputs. It’s good
-          business when your food production uses less resources, farmers are{" "}
-          <span className="bold">more in control</span> over their economics and
-          less reliant on long supply chains.
-        </div>
-
-        <div className="text">
-          At Listt we provide the appropriate technology solutions for
-          regeneration, and prioritise soil health while building thriving food
-          production businesses. From traditional IT solutions to connected
-          sensors for measuring soil and crop health, or even advanced
-          automation using robotics,
+          It’s good business when your food production uses less resources,
+          farmers and growers are <span className="bold">more in control</span>{" "}
+          over their economics and less reliant on long supply chains.
         </div>
       </div>
     </div>
