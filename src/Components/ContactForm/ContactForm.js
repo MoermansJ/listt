3d783bd5d6393a96921d06431cd5c7d1b681b49b
MoermansJ <Jonathan.moermans2@gmail.com>
2022-11-27 18:23:42 +0100

    contact form mockup done

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -1,67 +1,90 @@
-import "./ContactForm.css"
-import { useRef, useState } from "react"
+import "./ContactForm.css";
+import { useRef, useState } from "react";
 
 function ContactForm() {
-	const formRef = useRef();
-	const submitRef = useRef();
+  const formRef = useRef();
+  const submitRef = useRef();
 
-	const sendEmail = e => {
-		e.preventDefault();
+  const sendEmail = e => {
+    e.preventDefault();
 
-		// emailjs
-		// 	.sendForm(
-		// 		"service_vt9g7uk",
-		// 		"template_u8n0mo6",
-		// 		formRef.current,
-		// 		"eEXc6xmI1Id-Ggl_0"
-		// 	)
-		// 	.then(
-		// 		result => {
-		// 			console.log(result.text);
-		// 		},
-		// 		error => {
-		// 			console.log(error.text);
-		// 		}
-		// 	);
-	};
+    // emailjs
+    // 	.sendForm(
+    // 		"service_vt9g7uk",
+    // 		"template_u8n0mo6",
+    // 		formRef.current,
+    // 		"eEXc6xmI1Id-Ggl_0"
+    // 	)
+    // 	.then(
+    // 		result => {
+    // 			console.log(result.text);
+    // 		},
+    // 		error => {
+    // 			console.log(error.text);
+    // 		}
+    // 	);
+  };
 
-	const [formInput, setFormInput] = useState({
-		from_name: "",
-		from_email: "",
-		subject: "",
-		message: "",
-	});
+  const [formInput, setFormInput] = useState({
+    from_name: "",
+    from_email: "",
+    subject: "",
+    message: "",
+  });
 
-	const handleChange = e => {
-		const propertyKey = e.target.getAttribute("name");
-		const propertyValue = e.target.value;
+  const handleChange = e => {
+    const propertyKey = e.target.getAttribute("name");
+    const propertyValue = e.target.value;
 
-		setFormInput({ ...formInput, [propertyKey]: propertyValue });
-	};
+    setFormInput({ ...formInput, [propertyKey]: propertyValue });
+  };
 
-	return (
-		<form
-			id="contact-form"
-			ref={formRef}
-			onSubmit={sendEmail}
-			onChange={handleChange}
-		>
+  return (
+    <form
+      id="contact-form"
+      ref={formRef}
+      onSubmit={sendEmail}
+      onChange={handleChange}
+    >
+      <div className="form-grid">
+        <label htmlFor="name">Full Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your full name"
+          required
+        />
 
-			<div className="form-grid">
-				<label htmlFor="name">NAME</label>
-				<input type="text" id="name" name="name" required />
-				<label htmlFor="email">E-MAIL</label>
-				<input type="email" id="email" name="email" required />
-				<label htmlFor="message">MESSAGE</label>
-				<textarea id="message" name="message" required />
-				<button type="submit" id="submit" name="submit">SUBMIT</button>
-			</div >
+        <label htmlFor="company">Company</label>
+        <input
+          type="text"
+          id="company"
+          name="company"
+          placeholder="Your company name"
+        />
 
-		</form >
-	)
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          placeholder="Your phone number"
+        />
+
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Your message"
+          required
+        />
+        <button type="submit" id="submit" name="submit">
+          SUBMIT
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default ContactForm
-
-
-
+export default ContactForm;
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,17 +1,14 @@
-import "./Contact.css"
+import "./Contact.css";
 import ContactForm from "../../Components/ContactForm/ContactForm";
 
 function Contact() {
-	return (
-		<div className="contact-grid">
-			{/* <img src={"./images/green.jpg"} className="banner" alt="banner" /> */}
-			<div className="contact-form"><ContactForm /></div>
-		</div>
-	);
+  return (
+    <div className="contact-grid">
+      <div className="contact-form">
+        <ContactForm />
+      </div>
+    </div>
+  );
 }
-
-// image source: Photo by Quang Nguyen Vinh: https://www.pexels.com/photo/bunch-of-bok-choi-2518893/
-// Photo by Anna Shvets: https://www.pexels.com/photo/crop-gardener-with-topsoil-on-hand-5231239/
-// sunflowers Photo by Lisa Gromova: https://www.pexels.com/photo/a-field-of-sunflowers-13076947/
 
 export default Contact;
