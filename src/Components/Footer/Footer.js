efc8398f09a7aa9d71136b6cde81c6b4562a808d
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2022-12-01 17:33:46 +0100

    laptop backup

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,26 +4,29 @@ import ProjectsData from "../../Data/ProjectsData"
 function Footer() {
 	return (
 		<div className="footer-flex">
-			<div className="footer-socials">
-				<div>Social</div>
+			<div className="footer-social">
+				<div className="gray">Social</div>
 				<div>Twitter, facebook, linkedin ... icons</div>
 			</div>
 			<div className="footer-project">
-				<div>Latest project</div>
-				<div>{ProjectsData[0].title}</div>
+				<div className="text gray">Latest project</div>
+				<div className="subtitle">{ProjectsData[0].title}</div>
+				<div className="text gray">{ProjectsData[0].description}</div>
 			</div>
 			<div className="footer-contact">
-				<div>
-					<div>telephone</div>
+				<div className="text">
+					<div className="gray">Telephone</div>
 					<div>012 345 678</div>
 				</div>
-				<div>
-					<div>e-mail</div>
-					<div>abc@def.ghi</div>
+				<div className="text">
+					<div className="gray">E-mail</div>
+					<div>contact@listt.io</div>
 				</div>
-				<div>
-					<div> address</div>
-					<div>Parkstreet 1</div>
+				<div className="text">
+					<div className="gray">Address</div>
+					<div>9882 South Street</div>
+					<div>London</div>
+					<div>EC48 1RF</div>
 				</div>
 			</div>
 		</div >
